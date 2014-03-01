b01687237edcf08acb932d7229a60b157f756547
jij3x <jijex@yahoo.com>
2014-02-28 16:24:41 -0800

    Update RubiksCubeTest.java

--- RubiksCubeTest.java
+++ RubiksCubeTest.java
@@ -1,8 +1,7 @@
 public class RubiksCubeTest {
 
-	@Test
-	public void test() {
-		HashMap<Color, Color[][]> origCube = new HashMap<Color, Color[][]>();
+	private RubiksCube createCube() {
+		HashMap<Color, Color[][]> newCube = new HashMap<Color, Color[][]>();
 		Color[][] green = new Color[3][3];
 		Color[][] blue = new Color[3][3];
 		Color[][] red = new Color[3][3];
@@ -20,8 +19,66 @@ public class RubiksCubeTest {
 			}
 		}
 
-		RubiksCube testingCube = new RubiksCube(origCube);
-		assertTrue("", testingCube.isSolvedCube());
+		return new RubiksCube(newCube);
+	}
 
+	@Test
+	public void test() {
+		RubiksCube testingCube = createCube();
+		assertTrue(testingCube.isSolvedCube());
+
+		ArrayList<Move> steps = new ArrayList<Move>();
+		steps.add(Move.RightClockwise);
+		steps.add(Move.RightCounterCW);
+		assertTrue(testingCube.isSolvedCube());
+		steps.clear();
+		steps.add(Move.RightFlip);
+		steps.add(Move.RightFlip);
+		assertTrue(testingCube.isSolvedCube());
+
+		steps.clear();
+		steps.add(Move.LeftClockwise);
+		steps.add(Move.LeftCounterCW);
+		assertTrue(testingCube.isSolvedCube());
+		steps.clear();
+		steps.add(Move.LeftFlip);
+		steps.add(Move.LeftFlip);
+		assertTrue(testingCube.isSolvedCube());
+
+		steps.clear();
+		steps.add(Move.FrontClockwise);
+		steps.add(Move.FrontCounterCW);
+		assertTrue(testingCube.isSolvedCube());
+		steps.clear();
+		steps.add(Move.FrontFlip);
+		steps.add(Move.FrontFlip);
+		assertTrue(testingCube.isSolvedCube());
+
+		steps.clear();
+		steps.add(Move.BackClockwise);
+		steps.add(Move.BackCounterCW);
+		assertTrue(testingCube.isSolvedCube());
+		steps.clear();
+		steps.add(Move.BackFlip);
+		steps.add(Move.BackFlip);
+		assertTrue(testingCube.isSolvedCube());
+
+		steps.clear();
+		steps.add(Move.TopClockwise);
+		steps.add(Move.TopCounterCW);
+		assertTrue(testingCube.isSolvedCube());
+		steps.clear();
+		steps.add(Move.TopFlip);
+		steps.add(Move.TopFlip);
+		assertTrue(testingCube.isSolvedCube());
+
+		steps.clear();
+		steps.add(Move.BottomClockwise);
+		steps.add(Move.BottomCounterCW);
+		assertTrue(testingCube.isSolvedCube());
+		steps.clear();
+		steps.add(Move.BottomFlip);
+		steps.add(Move.BottomFlip);
+		assertTrue(testingCube.isSolvedCube());
 	}
 }
